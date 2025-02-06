3782fc353e17ea870412cbe352f041d8d5a4a1bb
jiwon <wb001026@naver.com>
2025-02-06 16:56:54 +0900

    🔄 업로드: 최신 분석 결과 반영

--- upload_to_github.sh
+++ upload_to_github.sh
@@ -4,7 +4,7 @@
 git remote add origin https://github.com/00jiwon/wine_analysis_project.git  # 🔴 여기에 본인 GitHub 저장소 URL 입력
 
 # Git 작업 수행
-git add analyze.py run_analysis.sh upload_to_github.sh output.txt log.txt environment.yml
+git add analyze.py run_analysis.sh upload_to_github.sh output.txt log.txt environment.yml README.md
 git commit -m "🔄 업로드: 최신 분석 결과 반영"
 git push origin master
 
